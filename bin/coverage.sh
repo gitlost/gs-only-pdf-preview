ad3d09edd0001f4c7d60909234b0184f5ee78105
gitlost <burmartke@gmail.com>
2017-04-29 13:41:41 +0100

    Try suhosin.

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -8,6 +8,7 @@ mkdir -p cov
 
 WP_TESTS_DIR=$WP_TESTS_DIR phpunit --coverage-php cov/main.cov
 WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_disable_functions phpunit --coverage-php cov/disable_functions.cov
+WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_suhosin7 phpunit --coverage-php cov/suhosin7.cov
 
 wget https://phar.phpunit.de/phpcov.phar
 
