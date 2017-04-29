694ea5850e287b9f1212bc57680a1f9a7dea5b21
gitlost <burmartke@gmail.com>
2017-04-29 14:36:08 +0100

    Try suhosin from git.

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -6,6 +6,14 @@ WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
 
 mkdir -p cov
 
+rm -rf suhosin7
+git clone https://github.com/sektioneins/suhosin7
+cd suhosin7
+phpize
+./configure
+make
+cd ..
+
 WP_TESTS_DIR=$WP_TESTS_DIR phpunit --coverage-php cov/main.cov
 WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_disable_functions phpunit --coverage-php cov/disable_functions.cov
 WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_suhosin7 phpunit --coverage-php cov/suhosin7.cov
