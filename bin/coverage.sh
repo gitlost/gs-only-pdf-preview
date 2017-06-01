c744ea1900673cad26c04243e65df9fd2046fc3b
gitlost <burmartke@gmail.com>
2017-06-01 01:22:27 +0100

    Add WP 4.7.4 to travis; update phpunit6-compat.

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -4,6 +4,7 @@ set -ex
 
 WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
 
+rm -rf cov
 mkdir -p cov
 
 rm -rf suhosin7
@@ -16,9 +17,12 @@ cd ..
 
 WP_TESTS_DIR=$WP_TESTS_DIR phpunit --exclude-group gopp-uninstall --coverage-php cov/main.cov
 WP_TESTS_DIR=$WP_TESTS_DIR phpunit --group gopp-uninstall --coverage-php cov/uninstall.cov
+
 WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_disable_functions phpunit --exclude-group gopp-uninstall --coverage-php cov/disable_functions.cov
 WP_TESTS_DIR=$WP_TESTS_DIR PHPRC=ini_suhosin7 phpunit --exclude-group gopp-uninstall --coverage-php cov/suhosin7.cov
 
 wget https://phar.phpunit.de/phpcov.phar
 
 php phpcov.phar merge cov --clover clover.xml && bash <(curl -s https://codecov.io/bash)
+
+rm -rf suhosin7
--- tests/phpunit6-compat.php
+++ tests/phpunit6-compat.php
@@ -1,10 +1,11 @@
 <?php
-// From https://core.trac.wordpress.org/attachment/ticket/39822/39822-2.patch
+// Based on https://core.trac.wordpress.org/attachment/ticket/39822
 
 if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
 	class_alias( 'PHPUnit\Framework\AssertionFailedError', 'PHPUnit_Framework_AssertionFailedError' );
 	class_alias( 'PHPUnit\Framework\BaseTestListener', 'PHPUnit_Framework_BaseTestListener' );
 	class_alias( 'PHPUnit\Framework\Error\Notice', 'PHPUnit_Framework_Error_Notice' );
+	class_alias( 'PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning' );
 	class_alias( 'PHPUnit\Framework\Exception', 'PHPUnit_Framework_Exception' );
 	class_alias( 'PHPUnit\Framework\ExpectationFailedException', 'PHPUnit_Framework_ExpectationFailedException' );
 	class_alias( 'PHPUnit\Framework\Test', 'PHPUnit_Framework_Test' );
